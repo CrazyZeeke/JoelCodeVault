5195967e2edba012719b4b879ad731069c035ebe
John Elia <joe9056@nyp.org>
2021-01-17 07:49:20 +0000

    updates on the server-scan app.

--- Server-Scan-prod.py
+++ Server-Scan-prod.py
@@ -2,7 +2,7 @@
 #
 # Script to scan folders and check the supplied file types for a string:
 # working.  need to add line numbers.
-# \\NYPMYAPPPRD16B.SIS.NYP.ORG\Apps\Root
+# 
 
 import time
 import os
@@ -12,17 +12,13 @@ ignored = {"two", "three", "seven"}  #folders to be ignored during search
 
 start_time = time.time() # We want to see how long the script executes.
 print("starting scan: ")
-#rootdir=('u:\\) # Mapped a Network Share to a local Drive (Z)
-rootdir=('\\\\JOEL-11787\\temp')
-#rootdir=('\\\\nypmynypuat16.sis.nyp.org\\D_SHARE_FULL\\sa') # Mapped a Network Share to a local Drive (Z)
-#rootdir=('\\\\NYPMYAPPPRD16B.SIS.NYP.ORG\\Apps\\Root') # Mapped a Network Share to a local Drive (Z)
 
+rootdir=('/home/temp') # /home/temp
 
 for folder, dirs, files in os.walk(rootdir):
 	for file in files:
 		if file.endswith(('.json','.xml','.htm','.html','.txt','.config','.cshtml','.asax','.asc','.ashx','.css','.js','.asmx','.aspx','.cs','.vb','.webinfo','.soap','.shtml')):
-			#fullpath = os.path.join(folder,file)
-			fullpath = [x for x in fullpath.join(folder,file) if x not in ignored]
+			fullpath = os.path.join(folder,file)
 			with open(fullpath, 'r',errors='ignore') as f:
 
 				line_count = 0 # initialize
@@ -37,5 +33,3 @@ for folder, dirs, files in os.walk(rootdir):
 print("Script Took : --- %s seconds --- " % round( (time.time() - start_time)))
 #######################################################################
 # folders = [x for x in os.listdir(path) if x not in ignored]
-#
-#
